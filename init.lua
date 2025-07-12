a5a63b467d7d5b024717749d468e038a0ed8cc83
matiyas <tougepl@gmail.com>
2025-07-12 19:44:38 +0200

    initial commit

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("lazy").setup({
+  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
+})
+
+vim.opt.colorcolumn = "120"
+vim.cmd("colorscheme tokyonight-night")
+vim.opt.relativenumber = false
