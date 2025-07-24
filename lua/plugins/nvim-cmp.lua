86d65d5f3b78595b60528c68189b5954d53fe7dc
matiyas <tougepl@gmail.com>
2025-07-25 00:01:04 +0200

    Half-working vue configuration

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,6 +1,10 @@
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
     "roobert/tailwindcss-colorizer-cmp.nvim",
     "onsails/lspkind-nvim",
   },
