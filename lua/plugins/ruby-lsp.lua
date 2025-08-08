d09d3fdc5f5a3df47d0df796c1e54c0d05279ee4
Damian Matyjaszek <dmatyjaszek@netkodo.com>
2025-08-08 09:22:41 +0200

    Fix solargraph timeout issue

---
+++ lua/plugins/ruby-lsp.lua
@@ -0,0 +1,29 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      solargraph = {
+        cmd = { "solargraph", "stdio" },
+        filetypes = { "ruby" },
+        root_dir = require("lspconfig.util").root_pattern("Gemfile", ".git", "Rakefile"),
+        settings = {
+          solargraph = {
+            diagnostics = true,
+            completion = true,
+            hover = true,
+            formatting = true,
+            symbols = true,
+            definitions = true,
+            rename = true,
+            references = true,
+            folding = true,
+          },
+        },
+        on_attach = function(client, bufnr)
+          -- Increase timeout for Ruby files
+          vim.lsp.buf_request_sync(bufnr, "initialize", {}, 10000)
+        end,
+      },
+    },
+  },
+}
