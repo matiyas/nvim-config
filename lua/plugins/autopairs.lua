c5e27b35a774ec95bf0cd3f884e6134a817dbf9d
matiyas <tougepl@gmail.com>
2026-02-09 08:41:17 +0100

    Add nvim-autopairs plugin.

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,32 @@
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    local autopairs = require("nvim-autopairs")
+
+    autopairs.setup({
+      check_ts = true,
+      ts_config = {
+        lua = { "string" },
+        javascript = { "template_string" },
+      },
+      fast_wrap = {
+        map = "<M-e>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = [=[[%'%"%>%]%)%}%,]]=],
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "Search",
+        highlight_grey = "Comment",
+      },
+    })
+
+    -- Integration with nvim-cmp if available
+    local cmp_status_ok, cmp = pcall(require, "cmp")
+    if cmp_status_ok then
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end
+  end,
+}
