6df85e8d7c1add665d21bbb376760a60df275d78
matiyas <tougepl@gmail.com>
2026-02-11 00:04:20 +0100

    Update README to install solargraph correctly

--- lua/plugins/ruby-lsp.lua
+++ lua/plugins/ruby-lsp.lua
@@ -1,3 +1,20 @@
+-- Find solargraph executable
+local function get_solargraph_cmd()
+  -- Try user gem directory first
+  local handle = io.popen('ruby -e "puts Gem.user_dir"')
+  if handle then
+    local gem_dir = handle:read("*a"):gsub("%s+", "")
+    handle:close()
+
+    local solargraph_path = gem_dir .. "/bin/solargraph"
+    if vim.fn.executable(solargraph_path) == 1 then
+      return { solargraph_path, "stdio" }
+    end
+  end
+  -- Fallback to system solargraph
+  return { "solargraph", "stdio" }
+end
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
@@ -5,7 +22,7 @@ return {
       -- Disable rubocop LSP to avoid duplicate with solargraph
       rubocop = false,
       solargraph = {
-        cmd = { "solargraph", "stdio" },
+        cmd = get_solargraph_cmd(),
         filetypes = { "ruby" },
         root_dir = require("lspconfig.util").root_pattern("Gemfile", ".git", "Rakefile"),
         flags = {
