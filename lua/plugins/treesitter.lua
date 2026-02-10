d16defaf76e1c075290d782c960ed43735e558c0
matiyas <tougepl@gmail.com>
2026-02-10 23:02:11 +0100

    Update treesitter and README

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,39 +1,37 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "javascript",
-        "typescript",
-        "vue",
-        "html",
-        "css",
-        "json",
-        "yaml",
-        "markdown",
-        "bash",
-        "c",
-        "cpp",
-        "ruby",
-        "scss",
-        "tsx",
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-      matchup = {
-        enable = true,
-      },
-    })
-  end,
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "vimdoc",
+      "javascript",
+      "typescript",
+      "vue",
+      "html",
+      "css",
+      "json",
+      "yaml",
+      "markdown",
+      "bash",
+      "c",
+      "cpp",
+      "ruby",
+      "scss",
+      "tsx",
+    },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = {
+      enable = true,
+    },
+    matchup = {
+      enable = true,
+    },
+  },
 }
