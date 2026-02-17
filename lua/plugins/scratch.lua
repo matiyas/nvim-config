a377ef06e74c03c751ffb36ccf586b6d43c8be7a
Damian Matyjaszek <dmatyjaszek@netkodo.com>
2026-02-17 08:51:04 +0100

    Sort scratch.nvim filetypes by category

--- lua/plugins/scratch.lua
+++ lua/plugins/scratch.lua
@@ -1,4 +1,14 @@
 return {
   "LintaoAmons/scratch.nvim",
   event = "VeryLazy",
+  config = function()
+    require("scratch").setup({
+      filetypes = {
+        "html", "css", "scss", "js", "vue",
+        "json", "yml",
+        "sh", "py", "rb",
+        "md", "txt",
+      },
+    })
+  end,
 }
