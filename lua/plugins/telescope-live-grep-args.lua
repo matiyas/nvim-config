d035f3bf27f6ae26af6a6879edf8f4963882527c
matiyas <tougepl@gmail.com>
2026-02-19 21:34:23 +0100

    Apply NVIM_IGNORE_DIRS to live_grep_args extension

--- lua/plugins/telescope-live-grep-args.lua
+++ lua/plugins/telescope-live-grep-args.lua
@@ -2,6 +2,19 @@ return {
   "nvim-telescope/telescope-live-grep-args.nvim",
   dependencies = { "nvim-telescope/telescope.nvim" },
   config = function()
+    local ignore = require("config.ignore")
+    local glob_patterns = {}
+    for _, dir in ipairs(ignore.get_ignored_dirs()) do
+      table.insert(glob_patterns, "--glob=!" .. dir .. "/**")
+    end
+    require("telescope").setup({
+      extensions = {
+        live_grep_args = {
+          auto_quoting = true,
+          additional_args = glob_patterns,
+        },
+      },
+    })
     require("telescope").load_extension("live_grep_args")
   end,
 }
