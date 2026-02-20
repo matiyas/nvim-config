9055c99d7e268ee0e89ae253db9fdda71788a21f
matiyas <tougepl@gmail.com>
2026-02-20 22:51:39 +0100

    Increase telescope preview size to 60% width

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,29 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "master",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  'nvim-telescope/telescope.nvim',
+  branch = 'master',
+  dependencies = { 'nvim-lua/plenary.nvim' },
   opts = function()
-    local ignore = require("config.ignore")
+    local ignore = require('config.ignore')
+    local actions = require('telescope.actions')
     return {
       defaults = {
         file_ignore_patterns = ignore.telescope_patterns(),
+        layout_strategy = 'horizontal',
+        layout_config = {
+          horizontal = {
+            preview_width = 0.6,
+            width = 0.9,
+            height = 0.9,
+          },
+        },
         mappings = {
           i = {
-            ["<C-q>"] = require("telescope.actions").send_selected_to_qflist + require("telescope.actions").open_qflist,
-            ["<M-q>"] = require("telescope.actions").send_to_qflist + require("telescope.actions").open_qflist,
+            ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+            ['<M-q>'] = actions.send_to_qflist + actions.open_qflist,
           },
           n = {
-            ["<C-q>"] = require("telescope.actions").send_selected_to_qflist + require("telescope.actions").open_qflist,
-            ["<M-q>"] = require("telescope.actions").send_to_qflist + require("telescope.actions").open_qflist,
+            ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+            ['<M-q>'] = actions.send_to_qflist + actions.open_qflist,
           },
         },
       },
