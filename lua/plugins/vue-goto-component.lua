76add8dc81b2dd918b20b5f559c9beab5fbcd503
Damian Matyjaszek <dmatyjaszek@netkodo.com>
2026-02-25 22:00:16 +0100

    Use standalone vue-goto-component.nvim plugin from local directory

--- lua/plugins/vue-goto-component.lua
+++ lua/plugins/vue-goto-component.lua
@@ -1,14 +1,5 @@
 return {
-  dir = vim.fn.stdpath("config"),
+  dir = "~/Workspace/vue-goto-component.nvim",
   ft = { "vue" },
-  config = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "vue",
-      callback = function()
-        vim.keymap.set("n", "gd", function()
-          require("vue-goto-component").goto_definition()
-        end, { buffer = true, desc = "Go to definition (Vue)" })
-      end,
-    })
-  end,
+  opts = {},
 }
