b2611219414a0331d7c24978aef56a1f0a47d987
matiyas <tougepl@gmail.com>
2026-02-25 10:29:47 +0100

    Fix X12 syntax plugin loading

--- lua/plugins/x12-syntax.lua
+++ lua/plugins/x12-syntax.lua
@@ -1,4 +1,8 @@
 return {
   "impliedchaos/VIM-X12-Syntax",
   ft = "x12",
+  config = function(plugin)
+    vim.opt.rtp:append(plugin.dir)
+    vim.cmd("source " .. plugin.dir .. "/x12.vim")
+  end,
 }
