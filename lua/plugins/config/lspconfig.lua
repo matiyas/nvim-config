13e16dea3af525fbfb9356591b8338438e76698a
matiyas <tougepl@gmail.com>
2026-02-25 22:14:19 +0100

    Add vtsls config required by vue_ls

--- lua/plugins/config/lspconfig.lua
+++ lua/plugins/config/lspconfig.lua
@@ -1,17 +1,15 @@
 local lspconfig = require("lspconfig")
 
--- Configure volar for vue files
-lspconfig.volar.setup({
-  filetypes = { "vue", "typescript", "javascript", "typescriptreact", "javascriptreact" },
+-- Configure vtsls for TypeScript/JavaScript (required by vue_ls)
+lspconfig.vtsls.setup({
+  filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact", "vue" },
+})
 
-  -- optionally, override settings here
-  -- settings = { ... },
-
-  -- enable formatting
+-- Configure vue_ls (volar) for Vue files
+lspconfig.vue_ls.setup({
+  filetypes = { "vue" },
   on_attach = function(client, bufnr)
-    -- Enable formatting capability only for volar server
     if client.server_capabilities.documentFormattingProvider then
-      -- Format on save example:
       vim.api.nvim_create_autocmd("BufWritePre", {
         buffer = bufnr,
         callback = function()
