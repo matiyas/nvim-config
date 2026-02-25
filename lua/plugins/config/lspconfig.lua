24257e0be9b61cd9114fa45aff1efb32b4b43f80
Damian Matyjaszek <dmatyjaszek@netkodo.com>
2026-02-25 22:15:56 +0100

    Configure vue_ls with TypeScript SDK path from vtsls

--- lua/plugins/config/lspconfig.lua
+++ lua/plugins/config/lspconfig.lua
@@ -1,13 +1,36 @@
 local lspconfig = require("lspconfig")
 
+local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
+local vtsls_path = mason_packages .. "/vtsls/node_modules/@vtsls/language-server"
+
 -- Configure vtsls for TypeScript/JavaScript (required by vue_ls)
 lspconfig.vtsls.setup({
   filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact", "vue" },
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = mason_packages .. "/vue-language-server/node_modules/@vue/language-server",
+            languages = { "vue" },
+            configNamespace = "typescript",
+            enableForWorkspaceTypeScriptVersions = true,
+          },
+        },
+      },
+    },
+  },
 })
 
 -- Configure vue_ls (volar) for Vue files
 lspconfig.vue_ls.setup({
   filetypes = { "vue" },
+  init_options = {
+    typescript = {
+      tsdk = vtsls_path .. "/node_modules/typescript/lib",
+    },
+  },
   on_attach = function(client, bufnr)
     if client.server_capabilities.documentFormattingProvider then
       vim.api.nvim_create_autocmd("BufWritePre", {
