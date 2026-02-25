7fdbd4f711b6f9645e656ea376615e980d95c9f3
Damian Matyjaszek <dmatyjaszek@netkodo.com>
2026-02-25 22:23:24 +0100

    Defer vue-goto-component keymap to run after LazyVim keymaps

--- lua/plugins/vue-goto-component.lua
+++ lua/plugins/vue-goto-component.lua
@@ -8,9 +8,12 @@ return {
     vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(args)
         if vim.bo[args.buf].filetype == "vue" then
-          vim.keymap.set("n", "gd", function()
-            require("vue-goto-component").goto_definition()
-          end, { buffer = args.buf, desc = "Go to definition (Vue)" })
+          -- Defer to run after LazyVim sets up its keymaps
+          vim.defer_fn(function()
+            vim.keymap.set("n", "gd", function()
+              require("vue-goto-component").goto_definition()
+            end, { buffer = args.buf, desc = "Go to definition (Vue)" })
+          end, 100)
         end
       end,
     })
