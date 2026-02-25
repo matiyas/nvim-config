d150a5df8de86182c93e05e126d413da705b13a2
Damian Matyjaszek <dmatyjaszek@netkodo.com>
2026-02-25 22:21:56 +0100

    Use keys spec for vue-goto-component to override LSP keymap

--- lua/plugins/vue-goto-component.lua
+++ lua/plugins/vue-goto-component.lua
@@ -1,5 +1,15 @@
 return {
   dir = "~/Workspace/vue-goto-component.nvim",
   ft = { "vue" },
+  keys = {
+    {
+      "gd",
+      function()
+        require("vue-goto-component").goto_definition()
+      end,
+      ft = "vue",
+      desc = "Go to definition (Vue)",
+    },
+  },
   opts = {},
 }
