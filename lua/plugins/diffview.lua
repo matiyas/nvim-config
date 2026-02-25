513524543e7d6d773cb73cd547565525a3523e15
matiyas <tougepl@gmail.com>
2026-02-25 08:22:15 +0100

    Add debouncing to diffview stage/unstage to prevent git lock conflicts

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -3,6 +3,21 @@ return {
   dependencies = "nvim-lua/plenary.nvim",
   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
   config = function()
+    local debounce_timer = nil
+    local debounce_delay = 200 -- milliseconds
+
+    local function debounced_stage(callback)
+      if debounce_timer then
+        debounce_timer:stop()
+      end
+      debounce_timer = vim.defer_fn(function()
+        debounce_timer = nil
+        callback()
+      end, debounce_delay)
+    end
+
+    local actions = require("diffview.actions")
+
     require("diffview").setup({
       diff_binaries = false,
       enhanced_diff_hl = true,
@@ -11,6 +26,18 @@ return {
       view = {
         merge_tool = {
           layout = "diff3_mixed",
+        },
+      },
+      keymaps = {
+        file_panel = {
+          {
+            "n",
+            "s",
+            function()
+              debounced_stage(actions.toggle_stage_entry)
+            end,
+            { desc = "Stage/unstage the selected entry (debounced)" },
+          },
         },
       },
     })
