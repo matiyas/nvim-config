9dcbc3565dc974be91706b1e9056e03b68d5a0d3
matiyas <tougepl@gmail.com>
2026-02-26 08:45:17 +0100

    Use GitHub repo for ruby-namespace-copy.nvim plugin instead of local file

--- lua/plugins/ruby-namespace.lua
+++ lua/plugins/ruby-namespace.lua
@@ -1,10 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
+  "matiyas/ruby-namespace-copy.nvim",
+  ft = { "ruby" },
+  opts = {},
   keys = {
     {
       "<leader>yn",
       function()
-        require("ruby-namespace").copy_ruby_namespace()
+        require("ruby-namespace-copy").copy_namespace()
       end,
       desc = "Yank Ruby namespace",
       ft = "ruby",
